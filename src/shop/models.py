7acd2bdfd542ce08b28dbab9725a4bc67f105826
Yura192c <fpm.mironchiYU@bsu.by>
2023-11-29 18:08:00 +0300

    Initialize Item model

--- src/shop/models.py
+++ src/shop/models.py
@@ -1,2 +1,11 @@
 from django.db import models
 from django.urls import reverse
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def get_absolute_url(self):
+        return reverse("get_checkout_session_id", args=[self.pk])
