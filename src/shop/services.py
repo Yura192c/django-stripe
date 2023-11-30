494726e3b91bb38fdc8e779b6025a2af67d232a4
Yura192c <fpm.mironchiYU@bsu.by>
2023-11-30 15:22:21 +0300

    Add services fot getting session id

---
+++ src/shop/services.py
@@ -0,0 +1,54 @@
+from django.shortcuts import get_object_or_404
+from .models import Item, Order
+import stripe
+from django.conf import settings
+
+stripe.api_key = settings.STRIPE_SECRET_KEY
+
+
+def get_item_session_id(request, pk):
+    item = get_object_or_404(Item, id=pk)
+
+    session = stripe.checkout.Session.create(
+        # payment_method_types=['card'],
+        line_items=[{
+            'price_data': {
+                'currency': 'usd',
+                'product_data': {
+                    'name': item.name,
+                    'description': item.description,
+                },
+                'unit_amount': int(item.price * 100),
+            },
+            'quantity': 1,
+        }],
+        mode='payment',
+        success_url=request.build_absolute_uri('/success'),
+        cancel_url=request.build_absolute_uri(item.get_absolute_url()),
+    )
+    return session
+
+
+def get_order_session_id(request, pk):
+    order = get_object_or_404(Order, id=pk)
+
+    session = stripe.checkout.Session.create(
+        line_items=[
+            {
+                'price_data': {
+                    'currency': 'usd',
+                    'product_data': {
+                        'name': item.name,
+                        'description': item.description,
+                    },
+                    'unit_amount': int(item.price * 100),
+                },
+                'quantity': 1,
+            } for item in order.items.all()
+        ],
+        mode='payment',
+        success_url=request.build_absolute_uri('/success'),
+        cancel_url=request.build_absolute_uri(order.get_absolute_url()),
+    )
+
+    return session
